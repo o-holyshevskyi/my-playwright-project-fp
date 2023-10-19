846918b1bb0ddede8e6e7dbcfdcdc816b36880bd
oholyshevskyi <oleksandr.holyshevskyi@pcmicorp.com>
2023-10-19 15:04:23 +0300

    move the goto to the fixture

--- tests/fixtures/fixture.ts
+++ tests/fixtures/fixture.ts
@@ -1,5 +1,6 @@
 import { test as base } from '@playwright/test';
 import { LandingPage, landingPage } from '../../src/pages/landing-page';
+import * as F from 'fp-ts/function';
 
 type Fixture = {
     landingPage: LandingPage;
@@ -8,6 +9,9 @@ type Fixture = {
 export const test = base.extend<Fixture>({
     landingPage: async ({ page }, use) => {
         const landing = landingPage(page);
+        await F.pipe(
+            landing.goto(),
+        )();
         await use(landing);
         await page.close();
     }
